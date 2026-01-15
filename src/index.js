7cf04ebe1b9a599a76c3eebfb94aa94a30dfde04
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-15 22:01:12 +0530

    added nodecron for scheduling mailing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
-    
+    const cron = require('node-cron');
 const { sendBasicEmail } = require('./services/email-service');
 const setupAndStartServer = ()=>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    sendBasicEmail('support@admin.com',
-        'rohitdhankhar7347@gmail.com',
-        "surprise",
-        "Surprise  "
-    );
+    // sendBasicEmail('support@admin.com',
+    //     'tl0209@gmail.com',
+    //     "surprise",
+    //     "Surprise ke bare me soch na motuuuuu"
+    // );
+    
     app.listen(PORT,()=>{
 
         console.log("Server Started on port:",PORT);
+      cron.schedule('*/30 * * * * *', () => {
+  console.log('Running a task every 30 seconds:', new Date().toLocaleTimeString());
+});
+
+console.log('Cron job scheduled successfully, waiting for the first run.');
     });
 }
 setupAndStartServer();
