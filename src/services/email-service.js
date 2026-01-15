2c1adbd9ecf011f0071e4a7bccbedc6080ead1ee
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-15 22:01:49 +0530

    added nodecron for scheduling mailing

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,12 +1,17 @@
 const sender = require('../config/emailConfig');
 const sendBasicEmail =async (mailFrom,mailTo,mailSubject,mailBody)=>{
-    const response = await sender.sendMail({
+    try {
+         const response = await sender.sendMail({
         from:mailFrom,
         to:mailTo,
         subject:mailSubject,
         text:mailBody
     });
     console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+   
 }
 module.exports = {
     sendBasicEmail
