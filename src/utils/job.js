4d4b150d63181855adbd09c8552d1b7fedb2cb72
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-16 12:44:42 +0530

    ADDED send email to pending functionality

--- src/utils/job.js
+++ src/utils/job.js
@@ -3,6 +3,14 @@ const emailService = require('../services/email-service');
 const setupJobs = ()=>{
     cron.schedule('*/2 * * * *',async()=>{
         const response = await emailService.fetchPendingEmails();
+        response.forEach((email) => {
+            emailService.sendBasicEmail(
+                "ReminderService@airline.com",
+                email.recepientEmail,
+                email.subject,
+                email.content
+            )
+        });
         console.log(response);
     });
 }
